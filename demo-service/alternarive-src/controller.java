4d03518ef66c50eef9160ce2e2264a9750005d1d
mohitsethi <mohit.sethi@target.com>
2019-01-11 10:36:13 +0530

    added alternative src

---
+++ demo-service/alternarive-src/controller.java
@@ -0,0 +1,16 @@
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    private final Producer producer;
+
+    @Autowired
+    KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
---
+++ demo-service/alternarive-src/producer.java
@@ -0,0 +1,15 @@
+
+@Service
+public class Producer {
+
+    private static final Logger logger = LoggerFactory.getLogger(Producer.class);
+    private static final String TOPIC = "users";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        logger.info(String.format("#### -> Producing message -> %s", message));
+        this.kafkaTemplate.send(TOPIC, message);
+    }
+}
